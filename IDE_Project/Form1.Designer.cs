e6c94ded6ea79ae9061d737a29cdcca5d7058f2d
Abdullah Ergül <abdullahergul23@gmail.com>
2023-12-24 00:56:22 +0300

    Upload Files

--- IDE_Project/Form1.Designer.cs
+++ IDE_Project/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace IDE_Project {
-    partial class Form1 {
+    partial class Login_Page {
         /// <summary>
         ///  Required designer variable.
         /// </summary>
@@ -23,12 +23,163 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login_Page));
+            codeRichTextBox = new RichTextBox();
+            tree = new TreeView();
+            ımageList1 = new ImageList(components);
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            newWindowItem = new ToolStripMenuItem();
+            openFolderItem = new ToolStripMenuItem();
+            exitItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            wievToolStripMenuItem = new ToolStripMenuItem();
+            runToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // codeRichTextBox
+            // 
+            codeRichTextBox.AcceptsTab = true;
+            codeRichTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            codeRichTextBox.BackColor = Color.FromArgb(31, 31, 31);
+            codeRichTextBox.BorderStyle = BorderStyle.None;
+            codeRichTextBox.Font = new Font("Consolas", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            codeRichTextBox.ForeColor = Color.White;
+            codeRichTextBox.Location = new Point(227, 41);
+            codeRichTextBox.Name = "codeRichTextBox";
+            codeRichTextBox.Size = new Size(769, 484);
+            codeRichTextBox.TabIndex = 1;
+            codeRichTextBox.Text = "";
+            codeRichTextBox.WordWrap = false;
+            codeRichTextBox.TextChanged += codeRichTextBox_TextChanged;
+            // 
+            // tree
+            // 
+            tree.BackColor = Color.FromArgb(24, 24, 24);
+            tree.BorderStyle = BorderStyle.None;
+            tree.ForeColor = Color.LightGray;
+            tree.Location = new Point(12, 41);
+            tree.Name = "tree";
+            tree.Size = new Size(209, 484);
+            tree.TabIndex = 2;
+            tree.NodeMouseClick += tree_NodeMouseClick;
+            // 
+            // ımageList1
+            // 
+            ımageList1.ColorDepth = ColorDepth.Depth32Bit;
+            ımageList1.ImageStream = (ImageListStreamer)resources.GetObject("ımageList1.ImageStream");
+            ımageList1.TransparentColor = Color.Transparent;
+            ımageList1.Images.SetKeyName(0, "folder-1484.png");
+            ımageList1.Images.SetKeyName(1, "document-64.png");
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem, wievToolStripMenuItem, runToolStripMenuItem, helpToolStripMenuItem, aboutToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1008, 24);
+            menuStrip1.TabIndex = 3;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newWindowItem, openFolderItem, exitItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // newWindowItem
+            // 
+            newWindowItem.Name = "newWindowItem";
+            newWindowItem.ShortcutKeys = Keys.Control | Keys.Shift | Keys.N;
+            newWindowItem.Size = new Size(220, 22);
+            newWindowItem.Text = "New Window";
+            newWindowItem.Click += newWindowItem_Click;
+            // 
+            // openFolderItem
+            // 
+            openFolderItem.Name = "openFolderItem";
+            openFolderItem.ShortcutKeys = Keys.Control | Keys.K;
+            openFolderItem.Size = new Size(220, 22);
+            openFolderItem.Text = "Open Folder";
+            openFolderItem.Click += openFolderItem_Click;
+            // 
+            // exitItem
+            // 
+            exitItem.Name = "exitItem";
+            exitItem.ShortcutKeys = Keys.Alt | Keys.F4;
+            exitItem.Size = new Size(220, 22);
+            exitItem.Text = "Exit";
+            exitItem.Click += exitItem_Click;
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(39, 20);
+            editToolStripMenuItem.Text = "Edit";
+            // 
+            // wievToolStripMenuItem
+            // 
+            wievToolStripMenuItem.Name = "wievToolStripMenuItem";
+            wievToolStripMenuItem.Size = new Size(44, 20);
+            wievToolStripMenuItem.Text = "View";
+            // 
+            // runToolStripMenuItem
+            // 
+            runToolStripMenuItem.Name = "runToolStripMenuItem";
+            runToolStripMenuItem.Size = new Size(40, 20);
+            runToolStripMenuItem.Text = "Run";
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(44, 20);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(52, 20);
+            aboutToolStripMenuItem.Text = "About";
+            // 
+            // Login_Page
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = Color.FromArgb(24, 24, 24);
+            ClientSize = new Size(1008, 537);
+            Controls.Add(tree);
+            Controls.Add(codeRichTextBox);
+            Controls.Add(menuStrip1);
+            Name = "Login_Page";
+            StartPosition = FormStartPosition.CenterScreen;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private RichTextBox codeRichTextBox;
+        private TreeView tree;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem wievToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem newWindowItem;
+        private ToolStripMenuItem exitItem;
+        private ToolStripMenuItem openFolderItem;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private ImageList ımageList1;
+        private ToolStripMenuItem runToolStripMenuItem;
     }
 }
--- IDE_Project/Form1.cs
+++ IDE_Project/Form1.cs
@@ -1,7 +1,149 @@
+using System.Text.RegularExpressions;
+
 namespace IDE_Project {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class Login_Page : Form {
+        public Login_Page() {
             InitializeComponent();
+        }
+
+        //private Rectangle codeRichTextBoxOriginalRectangle;
+        //private Rectangle originalFormsSize;
+        //private Rectangle treeOriginalRectangle;
+
+        private void Login_Page_Load(object sender, EventArgs e) {
+            //originalFormsSize = new Rectangle(this.Location.X, this.Location.Y, this.Width, this.Height);
+
+            //codeRichTextBoxOriginalRectangle = new Rectangle(codeRichTextBox.Location.X, codeRichTextBox.Location.Y, codeRichTextBox.Width, codeRichTextBox.Height);
+            //resizeControl(codeRichTextBoxOriginalRectangle, codeRichTextBox);
+
+            //treeOriginalRectangle = new Rectangle(tree.Location.X, tree.Location.Y, tree.Width, tree.Height);
+            //resizeControl(treeOriginalRectangle, tree);
+        }
+
+        //private void resizeControl(Rectangle r, Control c) {
+        //    float xRatio = (float)(this.Width) / (float)(originalFormsSize.Width);
+        //    float yRatio = (float)(this.Height) / (float)(originalFormsSize.Height);
+
+        //    int newX = (int)(r.Width * xRatio);
+        //    int newY = (int)(r.Height * yRatio);
+
+        //    int newWidth = (int)(r.Width * xRatio);
+        //    int newHeight = (int)(r.Height * yRatio);
+
+        //    c.Location = new Point(newX, newY);
+        //    c.Size = new Size(newWidth, newHeight);
+        //}
+
+        //private void Login_Page_Resize(object sender, EventArgs e) {
+        //    resizeControl(codeRichTextBoxOriginalRectangle, codeRichTextBox);
+        //    resizeControl(treeOriginalRectangle, tree);
+        //}
+
+        private void codeRichTextBox_TextChanged(object sender, EventArgs e) {
+            string keywords = @"\b(public|private|partial|static|namespace|class|using|void|foreach|in)\b";
+            MatchCollection keywordMatches = Regex.Matches(codeRichTextBox.Text, keywords);
+
+            string types = @"\b(Console)\b";
+            MatchCollection typeMatches = Regex.Matches(codeRichTextBox.Text, types);
+
+            string comments = @"(\/\/.+?$|\/\*.+?\*\/)";
+            MatchCollection commentMatches = Regex.Matches(codeRichTextBox.Text, comments, RegexOptions.Multiline);
+
+            string strings = "\".+?\"";
+            MatchCollection stringMatches = Regex.Matches(codeRichTextBox.Text, strings);
+
+            int originalIndex = codeRichTextBox.Text.Length;
+            int originalLength = codeRichTextBox.SelectionLength;
+            Color originalColor = Color.LightGray;
+
+            tree.Focus();
+
+            codeRichTextBox.SelectionStart = 0;
+            codeRichTextBox.SelectionLength = codeRichTextBox.Text.Length;
+            codeRichTextBox.SelectionColor = originalColor;
+
+            foreach (Match m in keywordMatches) {
+                codeRichTextBox.SelectionStart = m.Index;
+                codeRichTextBox.SelectionLength = m.Length;
+                codeRichTextBox.SelectionColor = Color.FromArgb(45, 122, 214);
+            }
+
+            foreach (Match m in typeMatches) {
+                codeRichTextBox.SelectionStart = m.Index;
+                codeRichTextBox.SelectionLength = m.Length;
+                codeRichTextBox.SelectionColor = Color.FromArgb(53, 177, 152);
+            }
+
+            foreach (Match m in commentMatches) {
+                codeRichTextBox.SelectionStart = m.Index;
+                codeRichTextBox.SelectionLength = m.Length;
+                codeRichTextBox.SelectionColor = Color.FromArgb(67, 138, 85);
+            }
+
+            foreach (Match m in stringMatches) {
+                codeRichTextBox.SelectionStart = m.Index;
+                codeRichTextBox.SelectionLength = m.Length;
+                codeRichTextBox.SelectionColor = Color.FromArgb(206, 118, 75);
+            }
+
+            codeRichTextBox.SelectionStart = originalIndex;
+            codeRichTextBox.SelectionLength = originalLength;
+            codeRichTextBox.SelectionColor = originalColor;
+
+            codeRichTextBox.Focus();
+            codeRichTextBox.ScrollToCaret();
+        }
+
+        private void newWindowItem_Click(object sender, EventArgs e) {
+            new Login_Page().Show();
+        }
+
+        private void exitItem_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void openFolderItem_Click(object sender, EventArgs e) {
+            folderBrowserDialog1.ShowDialog();
+            Cursor.Current = Cursors.WaitCursor;
+            tree.Nodes.Clear();
+
+            foreach (var item in Directory.GetDirectories(folderBrowserDialog1.SelectedPath)) {
+                DirectoryInfo di = new DirectoryInfo(item);
+                var node = tree.Nodes.Add(di.Name, di.Name, 0, 0);
+                node.Tag = di;
+            }
+
+            foreach (var item in Directory.GetFiles(folderBrowserDialog1.SelectedPath)) {
+                FileInfo fi = new FileInfo(item);
+                var node = tree.Nodes.Add(fi.Name, fi.Name, 1, 1);
+                node.Tag = fi;
+            }
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void tree_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e) {
+            if (e.Node.Tag == null) {
+                // return
+            }
+            else if (e.Node.Tag.GetType() == typeof(DirectoryInfo)) {
+                e.Node.Nodes.Clear();
+                foreach (var item in Directory.GetDirectories(((DirectoryInfo)e.Node.Tag).FullName)) {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(item);
+                    var node = e.Node.Nodes.Add(directoryInfo.Name, directoryInfo.Name, 0, 0);
+                    node.Tag = directoryInfo;
+                }
+
+                foreach (var item in Directory.GetFiles(((DirectoryInfo)e.Node.Tag).FullName)) {
+                    FileInfo fileInfo = new FileInfo(item);
+                    var node = e.Node.Nodes.Add(fileInfo.Name, fileInfo.Name, 1, 1);
+                    node.Tag = fileInfo;
+                }
+                e.Node.Expand();
+            }
+            else {
+                // open file
+                codeRichTextBox.Text = File.ReadAllText(((FileInfo)e.Node.Tag).FullName);
+            }
         }
     }
 }
